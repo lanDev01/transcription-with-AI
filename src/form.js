77bd5270f07ec6bf5383403b79d15685c3d3b851
Alan <lnjnr9215@gmail.com>
2023-08-19 21:24:55 -0300

    finalization of the project

--- src/form.js
+++ src/form.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { startLoading, stopLoading, loadingMessage } from './loading'
 import { getVideoId, loadVideo } from './youtube-api'
+import { transcribeAudio } from './transcribe'
+import { renderText } from './render'
 
 const form = document.querySelector('#form')
 
@@ -18,6 +20,8 @@ form.addEventListener('submit', async (e) => {
         loadingMessage('Baixando e convertando o video')
         await axios.get('http://localhost:3333/audio?v=' + getVideoId(url))
 
+        const data = await transcribeAudio()
+        renderText(data)
     } catch (error) {
         console.log('submit_error', error)
     } finally {
